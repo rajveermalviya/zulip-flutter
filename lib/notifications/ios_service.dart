0ec337524506fc20497b9260611cb946ba7259d8
Rajesh Malviya <rmalviya@zulip.com>
2026-02-17 18:52:22 +0530

    wip: Access database from Dart in Notification Service Extension
    Only pure-Dart/FFI based packages are working fine currently which
    includes package:drift.
    
    MethodChannel based Swift packages that use CocoaPods for building
    are not working.
    
    When attempting to configure CocoaPods for 'NotificationService'
    target, following errors (and possibly more, because second error
    was only thrown after I temporarily removed package:url_launcher)
    are thrown during the build process:
    
      Swift Compiler Error (Xcode): 'shared' is unavailable in application extensions for iOS: Use view controller based solutions where
      appropriate instead.
      /Users/rajveer/.pub-cache/hosted/pub.dev/url_launcher_ios-6.4.0/ios/url_launcher_ios/Sources/url_launcher_ios/Launcher.swift:24:25
      …
    
      Semantic Issue (Xcode): 'sharedApplication' is unavailable: not available on iOS (App Extension) - Use view controller based
      solutions where appropriate instead.
      /Users/rajveer/.pub-cache/hosted/pub.dev/wakelock_plus-1.4.0/ios/wakelock_plus/Sources/wakelock_plus/WakelockPlusPlugin.m:19:20
      …

--- lib/notifications/ios_service.dart
+++ lib/notifications/ios_service.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 
 import '../host/ios_notifications.g.dart';
+import '../model/binding.dart';
 
 @pragma('vm:entry-point')
 Future<void> iosNotificationServiceMain() async {
   print('dart: iosNotificationServiceMain');
   WidgetsFlutterBinding.ensureInitialized();
+  LiveZulipBinding.ensureInitialized();
 
   final iosNotifFlutterApiImpl = _IosNotifFlutterApiImpl();
   IosNotifFlutterApi.setUp(iosNotifFlutterApiImpl);
@@ -18,6 +20,9 @@ class _IosNotifFlutterApiImpl extends IosNotifFlutterApi {
   Future<MutatedNotificationContent> didReceivePushNotification(NotificationContent content) async {
     print("dart: _IosNotifFlutterApiImpl.didReceivePushNotification");
     print("dart: content.payload=${jsonEncode(content.payload)}");
+
+    final globalStore = await ZulipBinding.instance.getGlobalStore();
+    print('dart: globalStore.accounts=${globalStore.accounts}');
 
     final parsed = ApnsPayload.parseApnsPayload(content.payload);
 
